a9f9285095a1fbea59c936e427aff8358d1b9c99
Wei Xie <weixie@Weis-Mac-mini.local>
2021-08-24 15:46:31 -0400

    more problem, add docker file

---
+++ cpp_program/q1800_Maximum_Ascending_Subarray_Sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int n = nums.size();
+        int largest = 0;
+        int cur_sum = nums[0];
+        for (int i=1;i<n;i++){
+            largest = max(largest,cur_sum);
+            if (nums[i]<=nums[i-1]){
+                cur_sum = nums[i];
+            }
+            else{
+                cur_sum += nums[i];
+            }
+        }
+        largest = max(largest,cur_sum);
+        return largest;
+    }
+};
+[10,20,30,5,10,50]
+[10,20,30,40,50]
+[12,17,15,13,10,11,12]
+[100,10,1]
