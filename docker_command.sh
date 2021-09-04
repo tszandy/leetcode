bf5aa83c33046a4180a83104cd876343bff61df3
wei <444980834@qq.com>
2021-09-04 18:42:49 -0400

    add kali

--- docker_command.sh
+++ docker_command.sh
@@ -26,21 +26,24 @@ docker inspect -f ${container_name_or_id}
 #docker list ip address of running container ${container_name_or_id}
 docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${container_name_or_id}
 
-#docker run base ubuntu in detach mode --rm remove container when unrunning -d detach -t -i stdin interacitive
-docker run --rm -dti ubuntu:18.04
+#docker run base ubuntu in detach mode --rm remove container when unrunning -d detach -t tty protocol -i stdin interacitive
+docker run --rm -dti --name ubuntu ubuntu:18.04
 
-#docker run nvidia ubuntu runtime --gpus attached gpu 
+#docker run nvidia ubuntu runtime --gpus attached all gpu 
 docker run --rm --gpus all -dti nvidia/cuda:11.4.1-runtime-ubuntu18.04 /bin/bash
 
-#docker run nvidia ubuntu runtime --gpus attached gpu 
+#docker run nvidia ubuntu runtime --gpus attached all gpu 
 docker run --rm --gpus all -ti nvidia/cuda:11.4.1-runtime-ubuntu18.04 /bin/bash
 
 #rm all unrunning docker containers
 docker container prune
 docker rm $(docker ps -aq)
 
-#docker 
+# docker attach to a container with detach-key ctrl-a
 docker attach ${container_name_or_id} --detach-keys "ctrl-a"
+
+# docker execute command line in a container
+docker exec -it ${container_name_or_id} /bin/bash
 
 # docker network
 docker network ls
